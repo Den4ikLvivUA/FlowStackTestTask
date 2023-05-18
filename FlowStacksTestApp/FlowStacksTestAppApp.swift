ed750392d1011c716a0f4d461e3c182933deaf99
Denys Havryliak <den4iklvivua@gmail.com>
2023-05-18 10:38:43 +0300

    Initial Commit

---
+++ FlowStacksTestApp/FlowStacksTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStacksTestAppApp.swift
+//  FlowStacksTestApp
+//
+//  Created by Denys on 18.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowStacksTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
